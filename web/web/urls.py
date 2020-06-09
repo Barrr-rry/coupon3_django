2cb14d0bf9a4e0065eb286f9edd5d15a1e5b5b82
barry <barry@conquers.co>
2020-06-09 12:26:43 +0800

    chore: add staticfiles、add url

--- web/web/urls.py
+++ web/web/urls.py
@@ -21,13 +21,32 @@ from api import views
 from django.conf import settings
 from django.conf.urls.static import static
 from api import docs
-from .views import IndexView
+from .views import *
 
 urlpatterns = [
     path('api/', include(get_urls())),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('backend/conquers/admin/', admin.site.urls),
     path('', IndexView.as_view()),
+    path('404/', NotFoundView.as_view()),
+    path('addlisting/', AddlistingView.as_view()),
+    path('addlisting_2/', Addlisting2View.as_view()),
+    path('blog/', BlogView.as_view()),
+    path('blog-single/', BlogSingleView.as_view()),
+    path('comingsoon/', ComingsoonView.as_view()),
+    path('contact/', ContactView.as_view()),
+    path('explore-detail/', ExploreDetailView.as_view()),
+    path('explore-v1/', ExploreV1View.as_view()),
+    path('explore-v2/', ExploreV2View.as_view()),
+    path('explore-v3/', ExploreV3View.as_view()),
+    path('explore-v4/', ExploreV4View.as_view()),
+    path('explore-v5/', ExploreV5View.as_view()),
+    path('index2/', Index2View.as_view()),
+    path('index拷貝/', IndexCopyView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('price/', PriceView.as_view()),
+    path('register/', RegisterView.as_view()),
+
     # path('api/ecpay/payment/', views.PaymenetViewSet.as_view()),
     # path('api/ecpay/shipping/', views.ShippingViewSet.as_view()),
     # path('api/ecpay/redirect/', views.RedirectViewSet.as_view()),
--- web/web/views.py
+++ web/web/views.py
@@ -3,3 +3,79 @@ from django.views.generic.base import View, TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class NotFoundView(TemplateView):
+    template_name = '404.html'
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class AddlistingView(TemplateView):
+    template_name = 'addlisting.html'
+
+
+class Addlisting2View(TemplateView):
+    template_name = 'addlisting_2.html'
+
+
+class BlogView(TemplateView):
+    template_name = 'blog.html'
+
+
+class BlogSingleView(TemplateView):
+    template_name = 'blog-single.html'
+
+
+class ComingsoonView(TemplateView):
+    template_name = 'comingsoon.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+
+class ExploreDetailView(TemplateView):
+    template_name = 'explore-detail.html'
+
+
+class ExploreV1View(TemplateView):
+    template_name = 'explore-v1.html'
+
+
+class ExploreV2View(TemplateView):
+    template_name = 'explore-v2.html'
+
+
+class ExploreV3View(TemplateView):
+    template_name = 'explore-v3.html'
+
+
+class ExploreV4View(TemplateView):
+    template_name = 'explore-v4.html'
+
+
+class ExploreV5View(TemplateView):
+    template_name = 'explore-v5.html'
+
+
+class Index2View(TemplateView):
+    template_name = 'index2.html'
+
+
+class IndexCopyView(TemplateView):
+    template_name = 'index拷貝.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+
+class PriceView(TemplateView):
+    template_name = 'price.html'
+
+
+class RegisterView(TemplateView):
+    template_name = 'register.html'
