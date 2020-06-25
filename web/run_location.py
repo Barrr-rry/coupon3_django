13837a54c05d6160c2c2147ca7aa8ceab87a92b4
max <max@conquers.co>
2020-06-25 08:37:57 +0800

    chore: run location temp

--- web/run_location.py
+++ web/run_location.py
@@ -6,6 +6,7 @@ django.setup()
 import datetime
 import random
 import json
+import time
 from munch import AutoMunch
 from crawler import *
 
@@ -13,11 +14,39 @@ fmt = '%Y-%m-%d %H:%M:%S'
 test_email = 'max@conquers.co'
 
 
+def demo():
+    from strsimpy.damerau import Damerau
+    damerau = Damerau()
+    data = []
+    with open('./location.json') as f:
+        data = json.loads(f.read())
+    target = []
+    dct = dict()
+    for el in data:
+        raw_data = AutoMunch(el['raw_data'])
+        site_id = raw_data.site_id.replace(raw_data.city, '')
+        print(site_id)
+        dct[raw_data.road] = el
+        dct[f'{raw_data.city}{raw_data.road}'] = el
+        dct[f'{raw_data.site_id}{raw_data.road}'] = el
+    keys = list(dct.keys())
+    results = []
+    msg = '高雄市 文昌路3巷10號'
+    st = time.time()
+    for key in keys:
+        results.append((damerau.distance(key, msg), key))
+    results = sorted(results, key=lambda x: x[0])
+    ed = time.time()
+    print(ed - st)
+    print()
+
+
 def main():
     data = []
     with open('./location.json') as f:
         data = json.loads(f.read())
     target = []
+    dct = dict()
     for el in data:
         road = el['raw_data']['road']
         if road not in target:
@@ -36,4 +65,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    demo()
+    # main()
