474d405d15ee6e5f42879b03e8b87bfcf741221f
max <max@conquers.co>
2020-07-01 15:46:58 +0800

    chore: 調整lazy loading

--- web/web/templates/static/javascript/main.js
+++ web/web/templates/static/javascript/main.js
@@ -1927,6 +1927,25 @@ const showSelfPosition = (position) => {
       }, 1000)
     }
   }
+  let initLazyLoading = () => {
+    function onEnterView(entries, observer) {
+      for (let entry of entries) {
+        if (entry.isIntersecting) {
+          // 監視目標進入畫面
+          const img = entry.target
+          img.setAttribute('src', img.dataset.src) // 把值塞回 src
+          img.removeAttribute('data-src')
+          observer.unobserve(img) // 取消監視
+        }
+      }
+    }
+
+    const watcher = new IntersectionObserver(onEnterView)
+    const lazyImages = document.querySelectorAll('img.lazy')
+    for (let image of lazyImages) {
+      watcher.observe(image) // 開始監視
+    }
+  }
 
 
   $(function () {
@@ -1953,6 +1972,7 @@ const showSelfPosition = (position) => {
     contactUs();
     storePage();
     initToScroll();
+    initLazyLoading();
   });
 
 })(jQuery);
