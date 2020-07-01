deca4efbd71759f824edcf9fc2a394dec0f3f8b2
max <max@conquers.co>
2020-07-01 16:14:37 +0800

    fix: 商家上傳圖片時，應該建議圖片長寬、大小與格式
    fix #54

--- web/web/templates/static/javascript/main.js
+++ web/web/templates/static/javascript/main.js
@@ -1465,6 +1465,29 @@ const showSelfPosition = (position) => {
       })
     }
     setImageClick()
+    // image file 上傳前確認
+
+    $('input[name="upload-file"]').checkFileTypeAndSize({
+      allowedExtensions: ['jpg', 'png'],
+      maxSize: 10,
+      success: function () {
+      },
+      extensionerror: function () {
+        let msg = '允許的格式為.jpg .png'
+        Swal.fire({
+          text: msg,
+          confirmButtonText: '確定'
+        })
+      },
+      sizeerror: function () {
+        let msg = '超過 1MB，請重新上傳圖片'
+        Swal.fire({
+          text: msg,
+          confirmButtonText: '確定'
+        })
+      }
+    });
+
     $('input[name="upload-file"]').change((e) => {
       let form = new FormData()
       form.append("file", e.target.files[0])
@@ -1945,6 +1968,61 @@ const showSelfPosition = (position) => {
     for (let image of lazyImages) {
       watcher.observe(image) // 開始監視
     }
+  }
+
+
+  $.fn.checkFileTypeAndSize = function (options) {
+    //默认设置
+    var defaults = {
+      allowedExtensions: [],
+      maxSize: 1024, //单位是KB，默认最大文件尺寸1MB=1024KB
+      success: function () {
+      },
+      extensionerror: function () {
+      },
+      sizeerror: function () {
+      }
+    }
+    //合并设置
+    options = $.extend(defaults, options)
+    //遍历元素
+    return this.each(function () {
+      $(this).on('change', function () {
+        //获取文件路径
+        var filePath = $(this).val()
+        //小写字母的文件路径
+        var fileLowerPath = filePath.toLowerCase()
+        //获取文件的后缀名
+        var extension = fileLowerPath.substring(fileLowerPath.lastIndexOf('.') + 1)
+        //判断后缀名是否包含在预先设置的、所允许的后缀名数组中
+        if ($.inArray(extension, options.allowedExtensions) == -1) {
+          options.extensionerror()
+          $(this).focus()
+        } else {
+          try {
+            var size = 0
+            if (false) {//ie旧版浏览器
+              var fileMgr = new ActiveXObject("Scripting.FileSystemObject")
+              var fileObj = fileMgr.getFile(filePath)
+              size = fileObj.size //byte
+              size = size / 1024//kb
+              //size = size / 1024//mb
+            } else {//其它浏览器
+              size = $(this)[0].files[0].size//byte
+              size = size / 1024//kb
+              //size = size / 1024//mb
+            }
+            if (size > options.maxSize) {
+              options.sizeerror()
+            } else {
+              options.success()
+            }
+          } catch (e) {
+            alert("错误：" + e)
+          }
+        }
+      })
+    })
   }
 
 
