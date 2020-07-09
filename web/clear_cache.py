3b1e7bae3d06da3a8df1201ab8c21a7ed13a8457
max <max@conquers.co>
2020-07-09 16:21:52 +0800

    fix: cache 順序

--- web/clear_cache.py
+++ web/clear_cache.py
@@ -1,11 +1,10 @@
 from crawler import reids_wraper
 from run_init import *
 
-
+queryset = Store.objects.all()
+data = json.dumps(serializers.StoreSerializer(many=True, instance=queryset).data)
 for key in reids_wraper.r.keys():
     reids_wraper.r.delete(key)
 
-
-queryset = Store.objects.all()
-data = json.dumps(serializers.StoreSerializer(many=True, instance=queryset).data)
+key = 'store:cache'
 reids_wraper.set(key, data)
