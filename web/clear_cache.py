f4addad3356edb6331ffe0afd9a6075795491ec7
barry <barry@conquers.co>
2020-07-09 16:18:31 +0800

    chore: update clear_cache.py

--- web/clear_cache.py
+++ web/clear_cache.py
@@ -1,5 +1,11 @@
 from crawler import reids_wraper
+from run_init import *
 
 
 for key in reids_wraper.r.keys():
     reids_wraper.r.delete(key)
+
+
+queryset = Store.objects.all()
+data = json.dumps(serializers.StoreSerializer(many=True, instance=queryset).data)
+reids_wraper.set(key, data)
