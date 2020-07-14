baf0dac5693e09e543f486b2fecd652816b075c9
max <max@conquers.co>
2020-07-14 08:24:56 +0800

    fix: remove picture from batch

--- web/run_batch.py
+++ web/run_batch.py
@@ -11,3 +11,13 @@ with transaction.atomic():
     for el in querset:
         el.picture = None
         el.save()
+
+    queryset = StoreDiscount.objects.all()
+    for el in queryset:
+        if el.picture and len(el.picture) == 1:
+            el.picture = None
+            el.save()
+        store = Store.objects.filter(id=el.store_id)
+        if not store:
+            print(f'remove dis id: {el.id} store id: {el.store_id}')
+            # el.delete()
