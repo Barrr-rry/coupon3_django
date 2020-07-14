5e8afb1099dc6afe2910899260ec0fd906244c4e
max <max@conquers.co>
2020-07-14 08:42:01 +0800

    fix: remove picture from batch

--- web/run_batch.py
+++ web/run_batch.py
@@ -3,18 +3,9 @@ from django.db import transaction
 import json
 
 with transaction.atomic():
-    querset = StoreDiscount.objects.filter(picture='True')
-    for el in querset:
-        el.picture = None
-        el.save()
-    querset = StoreImage.objects.filter(picture='True')
-    for el in querset:
-        el.picture = None
-        el.save()
-
-    queryset = StoreDiscount.objects.all()
+    queryset = StoreImage.objects.all()
     for el in queryset:
-        if el.picture and len(el.picture) == 1:
+        if el.picture and (len(el.picture) == 1 or el.picture == 'True'):
             el.picture = None
             el.save()
         store = Store.objects.filter(id=el.store_id)
