a7c1da7b909c2fd72252e95766be179a565ed1ed
max <max@conquers.co>
2020-07-14 09:52:02 +0800

    fix: run batch

--- web/run_batch.py
+++ web/run_batch.py
@@ -2,13 +2,16 @@ from run_init import *
 from django.db import transaction
 import json
 
+desc = """【活動說明】：經政府公告，本網站可使用三倍券。
+
+【活動時間】：2020/7/15 ~ 2020/12/31。
+
+【活動網站】：https://3000.gov.tw/News.aspx?n=53&sms=9110 。"""
 with transaction.atomic():
-    queryset = StoreImage.objects.all()
+    queryset = Store.objects.filter(id__gte=12579, id__lte=12749)
+    queryset.delete()
+    queryset = Store.objects.filter(id__gte=12370, id__lte=12540)
     for el in queryset:
-        if el.picture and (len(el.picture) == 1 or el.picture == 'True'):
-            el.picture = None
-            el.save()
-        store = Store.objects.filter(id=el.store_id)
-        if not store:
-            print(f'remove dis id: {el.id} store id: {el.store_id}')
-            # el.delete()
+        dis = el.storediscount.first()
+        dis.description = desc
+        dis.save()
