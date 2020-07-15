9c0ae585e8d45d59b362234d651f1bd03a2dcc48
max <max@conquers.co>
2020-07-15 09:50:36 +0800

    fix: 活動網址 改變

--- web/run_batch.py
+++ web/run_batch.py
@@ -4,13 +4,11 @@ import json
 import os
 from PIL import Image
 
-"""
-如果 google place 有跑到資料的 先把 status改審核通過
-"""
 with transaction.atomic():
-    activity = Activity.objects.get(pk=27)
-    for el in activity.store.all():
-        if el.google_status == 1:
-            print(el.id)
-            el.status = 1
-            el.save()
+    queryset = StoreDiscount.objects.filter(description__icontains='活動網址')
+    for el in queryset:
+        desc = el.description
+        print(el.description)
+        desc = desc.replace('- 活動網址：', '【 活動網址 】：')
+        el.description = desc
+        el.save()
