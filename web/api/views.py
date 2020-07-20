b7065825dc7b105189741f674fd6959078546c5d
barry <barry@conquers.co>
2020-07-20 11:56:11 +0800

    fix: 假資料
    fix #205

--- web/api/views.py
+++ web/api/views.py
@@ -396,7 +396,7 @@ def to_column(el):
     if image:
         url = f'https://3coupon.info/media/{image.picture}'
     else:
-        url = f'https://3coupon.info/media/{el.image_1}'
+        url = f'https://3coupon.info/media/{el.store_type.replace_icon}'
     logger.warning(f'to col url: {url}')
     name = el.name
     uri = f'https://3coupon.info/store/{el.id}/'
