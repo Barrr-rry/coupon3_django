fe520075237ba8db4c9e527c41c81fc243a14388
barry <barry@conquers.co>
2020-07-21 16:27:10 +0800

    chore: update run_batch_barry.py

--- web/log.py
+++ web/log.py
@@ -19,3 +19,14 @@ logger.add(
     format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | PID:{process} |{message}",
     backtrace=True, diagnose=True
 )
+
+logger.add(
+    f'./logs/{datetime.date.today():%Y%m%d}_location_error.log',
+    rotation='1 day',
+    retention='7 days',
+    level='ERROR',
+    format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | PID:{process} |{message}",
+    backtrace=True, diagnose=True,
+    filter=lambda record: record["extra"].get("name") == "location"
+)
+
