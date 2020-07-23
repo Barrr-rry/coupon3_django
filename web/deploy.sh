a4763afb5b6e8ca6d13a3ddc1ff454d03cc28e9c
max <max@conquers.co>
2020-07-23 17:31:57 +0800

    fix: maintain

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker exec -it coupon3-django-web python before_deploy.py&\
-docker-compose -f docker-compose_test.yml up --build -d&\
+docker exec -it coupon3-django-web python before_deploy.py &
+docker-compose -f docker-compose_test.yml up --build -d &
 docker exec -it coupon3-django-web python after_deploy.py
