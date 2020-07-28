28e5c5185af97e8f634b28066564f998b21c4a56
barry <barry@conquers.co>
2020-07-28 17:07:27 +0800

    fix: line＠帳號改成新購買的
    fix #224

--- web/clear_real_delete_data.py
+++ web/clear_real_delete_data.py
@@ -5,7 +5,7 @@ from log import logger
 
 # 如果store 被刪掉 其他的img dis 等 都要一起被真的刪掉
 with transaction.atomic():
-    queryset = Store.original_objects.filter(deleted_status=True).all()
+    queryset = Store.original_objects.filter(deleted_status=True)
     for el in queryset:
         for img in el.storeimage.all():
             img.real_delete()
