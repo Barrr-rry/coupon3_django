a4189d26404a24e77265a2b6eb05efaa746e67b8
max <max@conquers.co>
2020-07-28 16:15:03 +0800

    docker: temp

--- web/clear_delete_data.py
+++ web/clear_delete_data.py
@@ -4,7 +4,7 @@ from django.db import transaction, router
 from log import logger
 
 # 如果store 被刪掉 其他的img dis 等 都要一起被刪掉
-for el in Store.objects.raw('SELECT id FROM api_store'):
+for el in Store.original_objects.raw('SELECT id FROM api_store'):
     print(el.location)
     # for img in el.storeimage.all():
     #     img.deleted_status = True
