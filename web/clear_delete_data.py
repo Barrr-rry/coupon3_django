43749c98425c8fee9c466b981aae424733b969c5
max <max@conquers.co>
2020-07-28 15:55:47 +0800

    docker: temp

--- web/clear_delete_data.py
+++ web/clear_delete_data.py
@@ -4,12 +4,13 @@ from django.db import transaction, router
 from log import logger
 
 # 如果store 被刪掉 其他的img dis 等 都要一起被刪掉
-with transaction.atomic():
-    queryset = Store.original_objects.filter(deleted_status=True)
-    for el in queryset:
-        for img in el.storeimage.all():
-            img.deleted_status = True
-            img.save()
-        for dis in el.storediscount.all():
-            dis.deleted_status = True
-            dis.save()
+# with transaction.atomic():
+queryset = Store.original_objects.filter(deleted_status=True)
+for el in queryset:
+    print(el.name)
+    # for img in el.storeimage.all():
+    #     img.deleted_status = True
+    #     img.save()
+    # for dis in el.storediscount.all():
+    #     dis.deleted_status = True
+    #     dis.save()
