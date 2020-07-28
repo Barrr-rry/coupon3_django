9b5ade281d82b820977a4e91d0dc15809473ad27
max <max@conquers.co>
2020-07-28 16:15:42 +0800

    docker: temp

--- web/clear_delete_data.py
+++ web/clear_delete_data.py
@@ -5,7 +5,11 @@ from log import logger
 
 # 如果store 被刪掉 其他的img dis 等 都要一起被刪掉
 for el in Store.original_objects.raw('SELECT id FROM api_store'):
-    print(el.location)
+    try:
+        a = el.location
+    except Exception as e:
+        print(el.id)
+
     # for img in el.storeimage.all():
     #     img.deleted_status = True
     #     img.save()
