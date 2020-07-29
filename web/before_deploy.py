b78ec7936cc530a9d8879504bcea5d3be06406e0
max <max@conquers.co>
2020-07-29 16:36:44 +0800

    chore: 註解修改

--- web/before_deploy.py
+++ web/before_deploy.py
@@ -32,6 +32,7 @@ def line_notify(msg):
 # 把template or 其他 所有的cache 都清除掉 常用來部署的時候 把cache 清除掉
 for key in reids_wraper.r.keys():
     reids_wraper.r.delete(key)
+# 更新狀態成 維護
 instance = ConfigSetting.objects.first()
 if not instance:
     ConfigSetting.objects.create(
